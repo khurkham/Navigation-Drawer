778c7e7842b8aa67497d9308db2f2f5a4ce4935a
khurkham <30972820+khurkham@users.noreply.github.com>
2018-02-21 23:21:24 +0700

    Initial commit

---
+++ app/src/main/java/com/andriod/khurkham/ebookstai/Books1Fragment.java
@@ -0,0 +1,33 @@
+package com.andriod.khurkham.ebookstai;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Books1Fragment extends Fragment {
+
+
+    public Books1Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_books1, container, false);
+    }
+
+}
+
